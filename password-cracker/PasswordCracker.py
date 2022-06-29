84ebe05a495b33b25fe5b9139d95f4ea3e1e0c72
Robert DeVore <deviodigital@gmail.com>
2022-06-29 19:50:08 -0400

    :ok_hand: IMPROVE: Updated script to loop through each txt file in 'lists' folder

--- password-cracker/PasswordCracker.py
+++ password-cracker/PasswordCracker.py
@@ -1,30 +1,36 @@
 import hashlib
 import time
+import fnmatch
+import os
 
 counter = 1
+t_time = int(0)
 
-md5_pass = input("[+] Enter Your md5 Pass : ")
-md5_file = input("[+] Enter your wordlist location : ")
+# Get the md5 pass to check against.
+md5_pass = input("[+] Enter Your md5 Pass: ")
 
-try:
-    md5_file = open(md5_file,"r")
-except:
-    print("\n File Not Found")
-    quit()
-
-for password in md5_file:
-    hash_obj = hashlib.md5(password.strip().encode('utf-8')).hexdigest()
-    start = time.time()
-    print("[-] Trying Password %d : %s " % (counter,password.strip()))
-    counter += 1
-    end = time.time()
-    t_time = end - start
-
-    if hash_obj == md5_pass:
-        print("\n[+] Password Found!!! Password Is : %s " % password)
-        print("[-] Total Ruuning Time is :  ",t_time,"seconds")
-        break
-
-else:
-    print("\n [X] Password not found.")
-    print("\n [-] Closing...")
+# Loop through files in 'lists' directory.
+for file in os.listdir('./lists'):
+    # Make sure we are only running on txt files.
+    if fnmatch.fnmatch(file, '*.txt'):
+        # Get the current file in the loop.
+        md5_file = open('./lists/' + file,"r")
+        # loop through eack line in txt file.
+        for password in md5_file:
+            hash_obj = hashlib.md5(password.strip().encode('utf-8')).hexdigest()
+            start = time.time()
+            print("[-] Trying Password %d: %s " % (counter,password.strip()))
+            counter += 1
+            end = time.time()
+            file_time = end - start
+            t_time += end - start
+            # We found a match.
+            if hash_obj == md5_pass:
+                print("\n[+] Password Found!!! Password Is: %s " % password)
+                print("[-] Total Running Time is:  ", file_time, "seconds")
+                break
+        # No match found.
+        else:
+            print("\n[X] Password match not found.")
+            print("[-] Closing...")
+            print("[-] Total Running Time is:  ",t_time,"seconds")
