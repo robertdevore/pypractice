56722598c5984affd195d09386643060dffa8205
Robert DeVore <deviodigital@gmail.com>
2023-06-19 20:00:16 -0400

    :ok_hand: IMPROVE: Updated Password Cracker with various fixes

--- password-cracker-2/passCracker.py
+++ password-cracker-2/passCracker.py
@@ -8,8 +8,6 @@ directory = os.path.dirname(os.path.realpath(__file__))
 # Define the hash to check against
 hash_to_check = input("Enter your hash: ")
 
-line_processed = 0
-
 # Define the available hash algorithms
 hash_algorithms = {
     "md5": hashlib.md5,
@@ -33,11 +31,10 @@ lines_processed = 0
 
 # Define a function that reads a text file and checks each password against the hash
 def process_file(file_path):
-
     global lines_processed
 
     # Get the selected hash algorithm
-    selected_algorithm = hash_algorithms[list(hash_algorithms.keys())[int(selection)-1]]
+    selected_algorithm = hash_algorithms[list(hash_algorithms.keys())[int(selection) - 1]]
 
     # Open the file in read mode with the latin-1 encoding
     with open(file_path, "r", encoding="latin-1") as f:
@@ -46,22 +43,25 @@ def process_file(file_path):
 
         # Loop through the file lines
         for password in file_lines:
+            # Remove leading/trailing whitespace from the password
+            password = password.strip()
+
             # Hash the password using the selected algorithm
             password_hash = selected_algorithm(password.encode()).hexdigest()
-
-            print(password)
 
             # Check if the password hash matches the hash to check
             if password_hash == hash_to_check:
                 # Print a message if the password hash matches
-                print(f"Found match for hash {hash_to_check} in file {file_path}: {password}")
-                break
+                print(f"Found match for hash {hash_to_check}: {password}")
+
+                # Set the flag to indicate a match has been found
+                return True
 
             # Increment the counter for the number of lines processed
             lines_processed += 1
 
-        # Return the number of lines processed
-        return lines_processed
+    # Return False if no match is found
+    return False
 
 if __name__ == '__main__':
     # Start the timer
@@ -69,6 +69,8 @@ if __name__ == '__main__':
 
     # Use os.walk() to loop through all the files in the directory
     # and all its subdirectories
+    match_found = False  # Flag to track if a match is found
+
     for root, dirs, files in os.walk(directory):
         # Loop through all the files
         for filename in files:
@@ -76,11 +78,11 @@ if __name__ == '__main__':
             if filename.endswith(".txt"):
                 # Get the full path to the file
                 file_path = os.path.join(root, filename)
-                # Use the try and except statements to catch any errors that occur
-                # during the execution of the script
                 try:
                     # Apply the process_file() function to the file
-                    lines_processed = process_file(file_path)
+                    if process_file(file_path):
+                        match_found = True
+                        break
                 except:
                     # Stop the timer
                     stop = time.perf_counter()
@@ -88,12 +90,18 @@ if __name__ == '__main__':
                     # Print a message with the time and number of lines processed
                     print(f"Process stopped early after {stop - start:0.4f} seconds and {lines_processed} lines processed")
                     break
+
+        if match_found:
             break
-        break
 
     # Stop the timer
     stop = time.perf_counter()
 
     # Print the time it took to run the script
     print(f"Finished in {stop - start:0.4f} seconds")
-    print(f"Processed {lines_processed} passwords. Found zero.")
+    print(f"Processed {lines_processed} passwords.")
+
+    if match_found:
+        print("Found at least one match.")
+    else:
+        print("Found zero matches.")
